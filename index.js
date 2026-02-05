0b9ce265450618be7a91c5bad5bf27e79e728b3b
Israel_Winnett <arulik98@gmail.com>
2026-02-05 16:35:26 +0200

    start with add event logic

--- index.js
+++ index.js
@@ -2,102 +2,100 @@ const targetScore = document.getElementsByClassName("target-score")
 const players = document.getElementsByClassName("player")
 const scores = document.getElementsByClassName("score")
 const totalScores = document.getElementsByClassName("total-score")
-const hold = document.getElementById("hold")
-const roll = document.getElementById("roll")
-const hold2 = document.getElementById("hold2")
-const roll2 = document.getElementById("roll2")
+const hold = document.getElementsByClassName("hold")
+const roll = document.getElementsByClassName("roll")
 
 
+hold[0].textContent = "Hold"
+roll[0].textContent = "Roll"
+hold[1].textContent = "Hold"
+roll[1].textContent = "Roll"
 
-hold.textContent = "Hold"
-roll.textContent = "Roll"
-hold2.textContent = "Hold"
-roll2.textContent = "Roll"
+// const player = [players[0], players[1]]
+
+let score = 0
+let totalScore = 0
+
+const player = {
+    player0: {
+        score,
+        totalScore
+    },
+    player1: {
+        score,
+        totalScore
+    }
+}
 
 
-
-let player1
-let player2
-let score = [0]
-let totalScore = [0]
-
+scores[0].textContent = `score: ${score}`
+scores[1].textContent = `score: ${score}`
+totalScores[0].textContent = `total: ${totalScore}`
+totalScores[1].textContent = `total: ${totalScore}`
 
 
-
-scores[0].textContent= `score: ${score}`
-scores[1].textContent= `score: ${score}`
-totalScores[0].textContent= `total: ${totalScore}`
-totalScores[1].textContent= `total: ${totalScore}`
-
+function notDuble(dice1, dice2, player) {
+    if (dice1 == dice2) {
+        score = 0
+        player[-1]
+    }
+    else {
+        score += dice1 + dice2
+        scores[player].textContent= score
+        totalScore += score
+        
+    }
+}
 
 
 function getRandDice(player) {
     const side1 = Math.floor(Math.random() * 6 + 1)
     const side2 = Math.floor(Math.random() * 6 + 1)
-    if (side1 == side2) {
-        if (player1) {
-            score = [0]                  
-            getRandDice(player2)
-        }
-        
-        if (player2) {
-            score = [0]
-            getRandDice(player1)
-        }
-    }
-    else score.push(side1 + side2)
-    return { side1, side2 };
+    notDuble(side1, side2)
+    // return { side1, side2 };
 }
 
 
 
 function RollOrHold() {
-    if (hold || hold2) {
-        hold.addEventListener("click", (e) => {
-            const target = e.target
-            if (player1) {
+    hold[0].addEventListener("click", (e) => {
+        let target = e.target
+        console.log(target);
+        target = player["player1"]
+        getRandDice(target)
 
+    })
+    hold[1].addEventListener("click", (e) => {
+        let target = e.target
+        console.log(target);
+        target = player["player0"]
+        getRandDice(target)
 
+    })
+    roll[0].addEventListener("click", (e) => {
+        let target = e.target
+        target = player["player1"]
+        getRandDice(target)
+    })
+    roll[1].addEventListener("click", (e) => {
+        let target = e.target
+        target = player["player1"]
+        getRandDice(target)
+    })
 
-            }
-            if (player2) {
-                getRandDice(player1)
-            }
-        })
-        hold2.addEventListener("click", (e) => {
-            if (player1) {
-                getRandDice(player2)
-            }
-            if (player2) {
-                getRandDice(player1)
-            }
-        })
-    }
-
-
-    if (roll || roll2) {
-        roll.addEventListener("click", (e) => {
-            const target = e.target
-            console.log("target roll", target);
-
-        })
-        roll2.addEventListener("click", (e) => {
-            return
-        })
-    }
 }
 
 
-
 function winner() {
-    if (totalScore[100]) {
+    if (totalScore = [50]) {
         return "we have a winner!"
     }
 }
 
 
+
+
+console.log(getRandDice(player0));
 RollOrHold()
-console.log(getRandDice(player1));
-console.log(score);
 
 
